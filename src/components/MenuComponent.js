8129a27a6a1e5bc703052c3d396dd7e8d76b4201
danh tran <tldanh1993@gmail.com>
2022-01-15 16:09:01 +0700

    change BS Media to Card

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+// import { Media } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from 'reactstrap';
 
 class Menu extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-
-    // };
+    this.state = {
+        
+    };
   }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div className="col-12 col-md-5 m-1" key={dish.id}>
+          <Card onClick={() => {}}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
 
     return (
       <div className="container">
-        <div className="row">
-          <Media list>
-            {/* menu component */}
-            {menu}
-          </Media>
-        </div>
+        <div className="row">{menu}</div>
       </div>
     );
   }
