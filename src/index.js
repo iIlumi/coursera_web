026061210f9ab33a64f20b09fd4436ffc4b451f0
danh tran <tldanh1993@gmail.com>
2022-01-15 15:41:45 +0700

    down to BS4 cdn, demo BS4 Media list component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// coursera use BS v4 but npm give BS5 -> import cnd vào index
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
