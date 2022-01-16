7b46acb2b2f4426c16ab9e4ce7e9bf28a71afd1e
danh tran <tldanh1993@gmail.com>
2022-01-16 21:09:28 +0700

    contact and temp home page rfc - rcc

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,27 +2,47 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+      // selectedDish: null,
     };
   }
 
-  onDishSelect = (dishId) => {
-    console.log('click selected');
-    this.setState({ selectedDish: dishId });
-  };
+  // onDishSelect = (dishId) => {
+  //   console.log('click selected');
+  //   this.setState({ selectedDish: dishId });
+  // };
 
   render() {
+    // Chỉ có 1 feature được chọn để show, find 1st hoặc filter chọn result[0]
+    // Đặt thếm homepage trung gian để viết gọn lại trong routes, xử lý truyền props gọn trước
+    
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.find((leader) => leader.featured)}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -42,13 +62,14 @@ export default class Main extends Component {
         /> */}
 
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<HomePage />} />
           <Route
             path="/menu"
             element={<Menu dishes={this.state.dishes} />}
             // component={() => <Menu dishes={this.state.dishes} />}
             // old router
           />
+          <Route path="/contactus" element={<Contact />} />
           <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
         <Footer />
