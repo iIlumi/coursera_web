085701e886ec3deacdf84227b93a9d40f5ba1a53
danh tran <tldanh1993@gmail.com>
2022-01-19 04:23:42 +0700

    >Redux thunk-logger- applyMiddleware, loading comp

--- src/redux/DishesReducer.js
+++ src/redux/DishesReducer.js
@@ -1,7 +1,25 @@
-import { DISHES } from '../shared/dishes';
+// import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const DishesReducer = (state = DISHES, action) => {
+export const DishesReducer = (
+  state = { isLoading: true, errMess: null, dishes: [] },
+  action
+) => {
   switch (action.type) {
+    case ActionTypes.DISHES_LOADING:
+      return { ...state, isLoading: true, errMess: null, dishes: [] };
+
+    case ActionTypes.DISHES_FAILED:
+      return { ...state, isLoading: false, errMess: action.payload };
+
+    case ActionTypes.ADD_DISHES:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        dishes: action.payload,
+      };
+
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,14 @@
 // import { Reducer, initialState } from './reducer';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { DishesReducer } from './DishesReducer';
 import { CommentsReducer } from './CommentsReducer';
 import { PromotionsReducer } from './PromotionsReducer';
 import { LeadersReducer } from './LeadersReducer';
 
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// https://github.com/reduxjs/redux-thunk#manual-setup
+// https://github.com/LogRocket/redux-logger#usage
 export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
@@ -12,7 +16,9 @@ export const ConfigureStore = () => {
       comments: CommentsReducer,
       promotions: PromotionsReducer,
       leaders: LeadersReducer,
-    })
+    }),
+    // Store enhancer
+    applyMiddleware(thunk, logger)
   );
 
   return store;
